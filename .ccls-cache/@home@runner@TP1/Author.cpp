3d5709d4714e15ac36a70decc2d24381abb1a184
geryaw <83832635+G3R74W@users.noreply.github.com>
2023-01-09 13:43:40 +0000

    fixed bug

--- .ccls-cache/@home@runner@TP1/Author.cpp
+++ .ccls-cache/@home@runner@TP1/Author.cpp
@@ -1,12 +1,12 @@
 #include "Author.h"
 
-Author(std::string firstName, std::string lastName, Date birth_date)
+Author::Author(std::string firstName, std::string lastName, Date birth_date)
     : _firstName(firstName), _lastName(lastName), _birth_date(birth_date) {
   // create author id
 }
 
 // getters
-std::string get_author_id() { return _author_id; }
+std::string Author::get_author_id() { return _author_id; }
 
 // operator overloading
 friend std::ostream &operator<<(std::ostream &os, const Author &a) {
